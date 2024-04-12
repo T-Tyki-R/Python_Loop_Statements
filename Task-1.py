4e07b23a38a13cc75b1ef47c6ee22dc56dbe7517
Tameka Roberson <TTRoberson@student.fullsail.edu>
2024-04-11 20:56:09 -0400

    Task 1 Completed

---
+++ Task-1.py
@@ -0,0 +1,15 @@
+import random
+# The Range Riddle 
+
+# initialize a list that stores moods
+# initialize a list that store the days of the week
+# loop through a week list using range and len methods
+# apply random method on the mood list
+# print the day of the week with the randomized mood
+
+moodsList = ["Happy", "Sad", "Neutral", "Upset", "Calm", "Energetic"]
+weekList = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+for day in range(len(weekList)):
+    randMood = random.choice(moodsList)
+    print(f"On {weekList[day]}, you were feeling {randMood}.")
