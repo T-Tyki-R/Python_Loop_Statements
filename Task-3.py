c9dce6c3066945d67742a6cf7905882fbf22049b
Tameka Roberson <TTRoberson@student.fullsail.edu>
2024-04-12 17:35:11 -0400

    Task 3 Started

---
+++ Task-3.py
@@ -0,0 +1,19 @@
+# Logic Conditions
+
+# Task 1
+
+# Create a while lopp with a never false condition
+
+i = 0
+
+while(i < 10):
+    print("Testing... testing...")
+    i += 1
+    if(i == 5):
+        break
+    
+    
+#
+#
+#
+#
