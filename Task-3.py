8af45ee4fa8f7bc1653dfc4b834d70df6afe449d
Tameka Roberson <TTRoberson@student.fullsail.edu>
2024-04-12 17:39:52 -0400

    Task 3 revised

--- Task-3.py
+++ Task-3.py
@@ -2,18 +2,18 @@
 
 # Task 1
 
-# Create a while lopp with a never false condition
+# Create a while lopp with a non-false condition (infinite loop)
+
+# Task 2
+# Modify the loop with a condition that will be true
 
 i = 0
 
-while(i < 10):
+while(True):
     print("Testing... testing...")
     i += 1
     if(i == 5):
         break
     
     
-#
-#
-#
-#
+
